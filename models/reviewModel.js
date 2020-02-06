48d96a0403298f9fba8ccefe58deec7574d9c583
Gittenger <john.pittenger86@gmail.com>
2020-02-06 03:54:28 -0500

    static method for calc avg rating post review save

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -51,6 +52,36 @@ reviewSchema.pre(/^find/, function(next) {
   });
 
   next();
+});
+
+//STATIC METHODS
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  //"this" points to the model directly since static method
+  //match all docs "tour" field (which is a tour Id), passed in via "this.tour"
+  //group by tour, calc stats
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  //assign calc'd stats to matching Tour doc
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating
+  });
+};
+
+//POST-SAVE MIDDLEWARE (NO ACCESS TO NEXT)
+//middleware to perform avg ratings calc every time (after) a new review is saved
+reviewSchema.post('save', function() {
+  //using this.constructor to access static method
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
