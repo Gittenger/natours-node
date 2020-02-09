2d6ef6be53cfa9d199268ad0d1f965d8c8f5b2b4
Gittenger <john.pittenger86@gmail.com>
2020-02-09 16:38:12 -0500

    added routes and controller for views, built dynamic overview page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const viewRouter = require('./routes/viewRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
@@ -81,27 +82,8 @@ app.use((req, res, next) => {
 });
 
 //ROUTES
-//VIEW ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    title: 'Exciting tours for exciting people',
-    tour: 'The Forest Hiker',
-    user: 'Jonas'
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
-  });
-});
-
+//VIEW ROUTER
+app.use('/', viewRouter);
 //API ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
