f26bb862391ab9da5b33212195498b94a9016ef8
Gittenger <john.pittenger86@gmail.com>
2020-02-10 23:36:18 -0500

    basic login functionality, check for session cookie in authController

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -48,6 +49,7 @@ app.use(
     limit: '10kb'
   })
 );
+app.use(cookieParser());
 
 //DEFEND AGAINST NOSQL QUERY INJECTION
 app.use(mongoSanitize());
@@ -78,6 +80,7 @@ app.use((req, res, next) => {
 //create request time field for use in responses
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const viewController = require('../controllers/viewController.js');
+const authController = require('../controllers/authController');
+
+const { protect } = authController;
 
 const { getOverview, getTour, getLogin } = viewController;
 
 const router = express.Router();
 
 router.get('/', getOverview);
-router.get('/tour/:slug', getTour);
+router.get('/tour/:slug', protect, getTour);
 router.get('/login', getLogin);
 
 module.exports = router;
