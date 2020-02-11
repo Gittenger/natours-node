0629ad41aca3e5fd7afc6a82db80ced6fd4ff8e1
Gittenger <john.pittenger86@gmail.com>
2020-02-10 23:03:46 -0500

    added basic login template

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const viewController = require('../controllers/viewController.js');
 
-const { getOverview, getTour } = viewController;
+const { getOverview, getTour, getLogin } = viewController;
 
 const router = express.Router();
 
 router.get('/', getOverview);
 router.get('/tour/:slug', getTour);
+router.get('/login', getLogin);
 
 module.exports = router;
