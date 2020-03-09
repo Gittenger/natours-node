0de9126c55ed9958d44233111b86caffe1b79d76
Gittenger <john.pittenger86@gmail.com>
2020-03-09 00:52:28 -0400

    stripe checkout  session working

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -11,6 +12,7 @@ const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 const userSettingsForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 //DELEGATION
 if (mapBox) {
@@ -69,5 +71,14 @@ if (userPasswordForm) {
     userPasswordForm.querySelector('#password-current').value = '';
     userPasswordForm.querySelector('#password').value = '';
     userPasswordForm.querySelector('#password-confirm').value = '';
+  });
+}
+
+if (bookBtn) {
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    console.log(tourId);
+    bookTour(tourId);
   });
 }
---
+++ public/js/stripe.js
@@ -0,0 +1,20 @@
+/*eslint-disable*/
+import axios from 'axios';
+import { showAlert } from './alert';
+const stripe = Stripe('pk_test_VhJKFk0tyZXeus91EoKmE3UC00RBBIFiZi');
+
+export const bookTour = async tourId => {
+  try {
+    //get checkout session from endpoint
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    //use stripe object to create checkout form & charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
