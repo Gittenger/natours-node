7859d0a4d5107c251e88e1d14b1ca14cb9896ee0
Gittenger <john.pittenger86@gmail.com>
2020-03-11 13:06:16 -0400

    added cors functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 ///////////////////////////////////////
 //Middlewares (GLOBAL)
+//IMPLEMENT CORS
+//allows all requests coming from all origins
+app.use(cors());
+app.options('*', cors());
+
 //SERVING STATIC FILES
 app.use(express.static(path.join(__dirname, 'public')));
 //SET SECURITY HTTP HEADERS
