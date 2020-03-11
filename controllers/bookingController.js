5f7f8729ee854d5353841a13265239420656588d
Gittenger <john.pittenger86@gmail.com>
2020-03-11 14:36:32 -0400

    debugging cont

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -65,7 +65,7 @@ const createBookingCheckout = async session => {
 };
 
 exports.webhookCheckout = (req, res, next) => {
-  const signature = req.headers('stripe-signature');
+  const signature = req.headers['stripe-signature'];
 
   let event;
   try {
